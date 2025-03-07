chirpdepin/chirp-token

Set address of the lockup pool
The lockup pool is the wallet that holds tokens penalized for early
withdrawal.

sources/pool_dispatcher.move
@@ -39,7 +39,7 @@ module blhnsuicntrtctkn::pool_dispatcher {
         dispatcher.pools.add(TEAM.to_string(), @0x6245be6621f4acb7fedb4ea2a1a25db6bee5ac4b19d37ef11848aa42d35155f8);
         dispatcher.pools.add(ADVISORS.to_string(), @0x69f0e03cd4f1f09e75e23362c15e07513effe7a01b18ac4bffbd5ac897bf53f0);
         dispatcher.pools.add(LIQUIDITY.to_string(), @0x8c305a5b03d873c3e030e355506787a57d11e03cb18d65c9be24af2dae3e7eda);
-        dispatcher.pools.add(LOCKUP.to_string(), @0xf984db9a25afa6c73aae9ba20ff9c43919ac717881d6dfbb7d0149b7888e8b42);
+        dispatcher.pools.add(LOCKUP.to_string(), @0xa65694ba9f7bc5370e532c9616d0a720d9843975fb55d21848ced91051a1ec03);
         return dispatcher
     }
 

chirpwireless/sui-testcoin-contract

sources/pool_dispatcher.move
@@ -39,7 +39,7 @@ module testcoin::pool_dispatcher {
         dispatcher.pools.add(TEAM.to_string(), @0xcd43e11fad9a01caad85159c9bf9aa02bbd5910eea2d6f95531d8ad1f1272ef7);
         dispatcher.pools.add(ADVISORS.to_string(), @0xee7c15989e4f071ef022b5ea27420e855c08395bec4998fc54820c9a682e84b7);
         dispatcher.pools.add(LIQUIDITY.to_string(), @0xeae12f80d9b462b60284984a3d6a216f4edcf4cc31c18842941c95fd030aa47f);
-        dispatcher.pools.add(LOCKUP.to_string(), @0xf984db9a25afa6c73aae9ba20ff9c43919ac717881d6dfbb7d0149b7888e8b42);
+        dispatcher.pools.add(LOCKUP.to_string(), @0xefaaecff5491118ffca9090afa473c53b937a5d2beb5b566c347bd386e5a656f);
         return dispatcher
     }
 